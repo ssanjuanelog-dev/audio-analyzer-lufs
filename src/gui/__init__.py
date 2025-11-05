3097bd47992a2937debfa45d1c10b92c973f15f1
ssanjuanelog-dev <s.sanjuanelog@gmail.com>
2025-11-05 18:26:15 -0500

    Create gui/__init__.py - GUI module package initialization

---
+++ src/gui/__init__.py
@@ -0,0 +1,9 @@
+"""
+GUI Module for Audio Analyzer LUFS Application
+
+This package contains all GUI components and interfaces for the audio analyzer.
+"""
+
+from .main_window import MainWindow
+
+__all__ = ['MainWindow']
